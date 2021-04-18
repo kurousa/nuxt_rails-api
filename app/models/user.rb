a5621d77fe15530fc83e9de5af06a0917b214acd
kurousa <r.takahashi.1026@gmail.com>
2021-04-18 21:17:04 +0900

    add_user_validation_and_ja.yml

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  # gem bcrypt
+  has_secure_password
+  # validates
+  validates :name, presence: true,
+                   length: { maximum: 30, allow_blank: true }
+
+  VALID_PASSWORD_REGEX = /\A[\w\-]+\z/
+  validates :password, presence: true,
+                       length: { minimum: 8 },
+                       format: {
+                         with: VALID_PASSWORD_REGEX,
+                         message: :invalid_password
+                       },
+                       allow_nil: true
+
+end
